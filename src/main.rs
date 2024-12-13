fcad5abf449888d74c803535ed3efff70af0d438
klion26 <qcx978132955@gmail.com>
2024-12-13 14:13:12 +0800

    split into mode

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-#![feature(type_ascription)]
 #![feature(future_join)]
 
+mod core;
+mod types;
+
+use crate::core::walker;
 extern crate clap;
 extern crate serde_json;
 
 use clap::Parser;
-use serde_json::Value;
 use std::fs::File;
 use std::io::Read;
 use std::string::String;
@@ -21,79 +23,6 @@ pub struct CommandArgs {
     selector: String,
     #[arg(short, long, default_value = "false")]
     pretty: bool,
-}
-
-type Selection = Result<Vec<Value>, String>;
-
-fn get_selection(json: &Value, selector: Option<&str>) -> Option<Selection> {
-    let mut inner_json = json;
-
-    if let Some(selector) = selector {
-        let selector: Vec<&str> = selector.split('.').collect();
-        // Returns Result of values or Err early on, stopping the iteration.
-        let items: Selection = selector
-            .iter()
-            .enumerate()
-            .map(|(i, s)| -> Result<Value, String> {
-                // let iter_index = i.to_string();
-                if let Ok(index) = s.parse::<isize>() {
-                    if (index as isize).is_negative() {
-                        Err(String::from("Invalid negative array index"))
-                    } else {
-                        if inner_json[index as usize] == Value::Null {
-                            let error_message = match inner_json.as_array() {
-                                Some(array) => [
-                                    "Index (",
-                                    s,
-                                    ") is out of bound, node (",
-                                    selector[i - 1],
-                                    ") has a length of",
-                                    &(array.len()).to_string(),
-                                ].join(" "),
-                                // Trying to acces an index on a node which
-                                // is not an arrya.
-                                None => [
-                                    "Node (",
-                                    selector[i - 1],
-                                    ") is not an array",
-                                ].join(" "),
-                            };
-                            println!("# {:?} #", inner_json.as_array());
-                            Err(error_message)
-                        } else {
-                            inner_json = &inner_json[index as usize];
-                            Ok(inner_json.clone())
-                        }
-                    }
-                } else {
-                    if s.is_empty() {
-                        Err(String::from("Unterminated selector found"))
-                    } else {
-                        if inner_json[s] == Value::Null {
-                            if i == 0 {
-                                Err(["Node (", s, ") is not the root element"]
-                                    .join(" "))
-                            } else {
-                                Err([
-                                    "Node (",
-                                    s,
-                                    ") not found on parent (",
-                                    selector[i - 1],
-                                    ")",
-                                ].join(" "))
-                            }
-                        } else {
-                            inner_json = &inner_json[s];
-                            Ok(inner_json.clone())
-                        }
-                    }
-                }
-            })
-            .collect();
-        Some(items)
-    } else {
-        None
-    }
 }
 
 fn main() {
@@ -117,7 +46,7 @@ fn main() {
                         serde_json::to_string_pretty(&valid_json).unwrap()
                     )
                 }
-                match get_selection(&valid_json, Some(selector.as_str())) {
+                match walker(&valid_json, Some(selector.as_str())) {
                     Some(items) => match items {
                         Ok(results) => println!(
                             "{}",
