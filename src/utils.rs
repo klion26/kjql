3de322a7014ca334b3f76233db9d8b299f2ff280
klion26 <qcx978132955@gmail.com>
2024-12-20 13:49:51 +0800

    clean code

--- src/utils.rs
+++ src/utils.rs
@@ -4,16 +4,6 @@ use std::io::prelude::Read;
 use std::io::BufReader;
 use toml::Value;
 
-pub fn get_cargo_version() -> String {
-    let cargo_file = File::open("Cargo.toml").unwrap();
-    let mut buffer_reader = BufReader::new(cargo_file);
-    let mut yaml = String::new();
-
-    buffer_reader.read_to_string(&mut yaml).unwrap();
-    let parsed = yaml.parse::<Value>().unwrap();
-    String::from((parsed["package"]["version"]).as_str().unwrap())
-}
-
 // convert a range to a readable string.
 fn display_range_selector(
     (start, end): (usize, usize),
